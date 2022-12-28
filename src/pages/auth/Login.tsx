d6f04bfd37c6f729f2619c2521237f34c32fd8aa
Raikonif <raikonif@gmail.com>
2022-12-28 15:03:11 -0400

    login design implemented

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,27 +1,62 @@
-import React from 'react'
-import {RiMailLine, RiLockLine} from "react-icons/ri";
+import React, { useState } from "react";
+import {
+    RiMailLine,
+    RiLockLine,
+    RiEyeLine,
+    RiEyeOffLine,
+} from "react-icons/ri";
+
 const Login = () => {
-  return (
-    
-    <div className='bg-white p-8 rounded-lg'>
-        <div className='mb-10'>
-          <h1 className='text-3x1 uppercase font-bold mb-4' >Sign In</h1>
-              <form>
-                  <div>
-                      <input 
-                      type='email' 
-                      className='w-full border border-green-200 outline-none py-2 px-4 rounded-lg mb-2' 
-                      id='email' 
-                      placeholder='E - mail' />
-                      <input type='password' 
-                      className='w-full border border-green-200 outline-none py-2 px-4 rounded-lg' 
-                      id='password' 
-                      placeholder='Password' />
-                  </div>
-              </form>
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className="bg-white p-8 rounded-lg">
+            <div className="mb-10">
+                <h1 className="text-3x1 uppercase font-bold mb-4">Sign In</h1>
+                <form className="flex flex-col gap-4">
+                    <div className="relative">
+                        <RiMailLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 " />
+                        <input
+                            type="email"
+                            className="w-full border border-green-200 outline-none py-2 px-8 rounded-lg mb-2"
+                            id="email"
+                            placeholder="E-mail"
+                        />
+                    </div>
+                    <div className="relative">
+                        <RiLockLine className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500" />
+                        <input
+                            type={showPassword ? "text" : "password"}
+                            className="w-full border border-green-200 outline-none py-2 px-8 rounded-lg"
+                            id="password"
+                            placeholder="Password"
+                        />
+                        {showPassword ? (
+                            <RiEyeOffLine
+                                onClick={handleShowPassword}
+                                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer" />
+                        ) : (
+                            <RiEyeLine
+                                onClick={handleShowPassword}
+                                className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:cursor-pointer" />
+                        )}
+                    </div>
+                    <div>
+                        <button
+                            className="bg-green-500 text-white py-2 px-6 rounded-lg w-full hover:bg-green-400 transition-colors duration-300"
+                            type="submit"
+                        >
+                            Sign In
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
 export default Login;
